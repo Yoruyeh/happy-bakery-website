da442d23c1926a02f5354a0965867607ae6704ca
Yoruyeh <orchardsky31@gmail.com>
2023-07-28 15:01:14 +0800

    feat: add layout, navbar and footer components

--- src/router/router.js
+++ src/router/router.js
@@ -1,37 +1,56 @@
 import * as React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, Outlet } from 'react-router-dom'
 import Home from '../pages/home/Home'
 import About from '../pages/about/About'
 import Login from '../pages/login/Login'
 import Register from '../pages/register/Register'
 import Contact from '../pages/contact/Contact'
 import Products from '../pages/products/Products'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
+const Layout = () => {
+  return (
+    <div className="main">
+      <Navbar />
+      <div className="container">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/register',
-    element: <Register />
-  },
-  {
-    path: '/about',
-    element: <About />
-  },
-  {
-    path: '/contact',
-    element: <Contact />
-  },
-  {
-    path: '/products',
-    element: <Products />
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/register',
+        element: <Register />
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      },
+      {
+        path: '/products',
+        element: <Products />
+      }
+    ]
   }
 ])
 
