14074dcf13df4e254d7be04ce0b4225a1dd29e7b
Yoruyeh <orchardsky31@gmail.com>
2023-07-29 10:38:23 +0800

    feat: add basename for router and write 404 redirect page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { RouterProvider } from 'react-router-dom'
 import router from './router/router'
 
+const basename = process.env.PUBLIC_URL
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider basename={basename} router={router} />
 }
 
 export default App
