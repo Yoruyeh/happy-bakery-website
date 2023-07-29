dddeb5702f71e25ddfaee09356aa8215ffbb48cb
Yoruyeh <orchardsky31@gmail.com>
2023-07-29 21:27:16 +0800

    feat: write tag component

--- src/components/card/ProductCard.jsx
+++ src/components/card/ProductCard.jsx
@@ -1,12 +1,14 @@
 import './productCard.scss'
 import ProdcutImg from '../../assets/images/strawberry-cake.jpeg'
 import Button from '../button/Button'
+import Tag from '../tag/Tag'
 
 const ProductCard = () => {
   return (
     <div className="card">
       <div className="card-image">
         <img src={ProdcutImg} alt="" />
+        <Tag text={'NEW'}/>
       </div>
       <div className="card-title">STRAWBERRY CAKE</div>
       <div className="card-button">
---
+++ src/components/tag/Tag.jsx
@@ -0,0 +1,7 @@
+import './tag.scss'
+
+const Tag = ({ text }) => {
+  return <div className="tag">{text}</div>
+}
+
+export default Tag
