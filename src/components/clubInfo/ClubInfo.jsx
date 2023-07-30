b0a81727eacdbd3f87abe6057370df463fc66c25
Yoruyeh <orchardsky31@gmail.com>
2023-07-30 15:57:03 +0800

    refactor: write clubInfo component and use in login and register page

---
+++ src/components/clubInfo/ClubInfo.jsx
@@ -0,0 +1,31 @@
+import { ArrowForward } from '../../assets/icons'
+import styles from './clubInfo.module.scss'
+import Button from '../button/Button'
+
+const ClubInfo = () => {
+  return (
+    <div className={styles.clubInfo}>
+      <div className={styles.clubInfoText}>
+        <h2>Join Happy Bakery Club Get Rewarded Today.</h2>
+        <p>
+          As Happy Bakery club member you get rewarded with what you love for
+          doing what you love. Sign up today and receive immediate access to
+          these Level 1 benefits:
+        </p>
+        <ul>
+          <li>Free shipping</li>
+          <li>A 15% off voucher for your next purchase</li>
+          <li>Access to Members Only products and sales</li>
+          <li>Special offers and promotions</li>
+        </ul>
+        <p>
+          Join now to start earning points, reach new levels and unlock more
+          rewards and benefits from adiClub.
+        </p>
+      </div>
+      <Button text={'JOIN THE CLUB'} price={<ArrowForward />} />
+    </div>
+  )
+}
+
+export default ClubInfo
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,5 +1,6 @@
 import styles from './register.module.scss'
 import { ArrowForward, FacebookColored } from '../../assets/icons'
+import ClubInfo from '../../components/clubInfo/ClubInfo'
 
 const Register = () => {
   return (
@@ -50,29 +51,7 @@ const Register = () => {
           <ArrowForward />
         </button>
       </form>
-      <div className={styles.clubInfo}>
-        <div className={styles.clubInfoText}>
-          <h2>Join Happy Bakery Club Get Rewarded Today.</h2>
-          <p>
-            As Happy Bakery club member you get rewarded with what you love for
-            doing what you love. Sign up today and receive immediate access to
-            these Level 1 benefits:
-          </p>
-          <ul>
-            <li>Free shipping</li>
-            <li>A 15% off voucher for your next purchase</li>
-            <li>Access to Members Only products and sales</li>
-            <li>Special offers and promotions</li>
-          </ul>
-          <p>
-            Join now to start earning points, reach new levels and unlock more
-            rewards and benefits from adiClub.
-          </p>
-        </div>
-        <button>
-          JOIN THE CLUB <ArrowForward />
-        </button>
-      </div>
+      <ClubInfo />
     </div>
   )
 }
