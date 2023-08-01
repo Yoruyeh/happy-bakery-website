6e9c2a0e87933aa333bdfdf6361da02b3a16fa0d
Yoruyeh <orchardsky31@gmail.com>
2023-08-01 18:01:55 +0800

    feat: add finish order page

--- src/pages/order/Finish.jsx
+++ src/pages/order/Finish.jsx
@@ -1,9 +1,27 @@
 import styles from './finish.module.scss'
+import BackgroundImg from '../../assets/images/cupcakes.jpeg'
+import HappyImg from '../../assets/images/happy.png'
+import { Link } from 'react-router-dom'
+import Button from '../../components/button/Button'
 
 const Finish = () => {
   return (
     <div className={styles.finish}>
-      Order confirmed
+      <div className={styles.bgImg}>
+        <img src={BackgroundImg} alt="" />
+      </div>
+      <div className={styles.text}>
+        <h1>Thank you for your purchase!</h1>
+        <img src={HappyImg} alt="" />
+        <div className={styles.link}>
+          <Link to="../member/orders">
+            <Button text={'Check Orders'} />
+          </Link>
+          <Link to="..">
+            <Button text={'Continue Shopping'} />
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
