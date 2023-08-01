26cfdbdd8da469556e6833aaff9e12b195584f38
Yoruyeh <orchardsky31@gmail.com>
2023-08-01 20:08:20 +0800

    feat: write our story(about) page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,44 @@
+import styles from './about.module.scss'
+import BackgroundImg1 from '../../assets/images/about-bg1.jpeg'
+import BackgroundImg2 from '../../assets/images/about-bg2.jpeg'
 
 
 const About = () => {
   return (
-    <div className="about">
-      <h1>About</h1>
+    <div className={styles.about}>
+      <div className={styles.bgImg1}>
+        <img src={BackgroundImg1} alt="" />
+        <div className={styles.text}>
+          <h1>Our Story</h1>
+          <p>
+            Cras auctor mollis cursus. Nulla facilisi. Curabitur vitae ipsum
+            mattis, finibus lectus ac, vehicula nisi. Duis est nisl, auctor
+            vitae placerat eu, vehicula quis ligula. Vestibulum mollis nisi
+            arcu, vitae sagittis magna placerat a. Class aptent taciti sociosqu
+            ad litora torquent per conubia nostra, per inceptos himenaeos.
+            Nullam sit amet cursus risus.
+          </p>
+          <p>
+            Vestibulum mollis nisi arcu, vitae sagittis magna placerat a. Class
+            aptent taciti sociosqu ad litora torquent per conubia nostra, per
+            inceptos himenaeos. Nullam sit amet cursus risus.
+          </p>
+        </div>
+      </div>
+      <div className={styles.bgImg2}>
+        <img src={BackgroundImg2} alt="" />
+        <div className={styles.text}>
+          <h1>100% Natural & Healthy</h1>
+          <p>
+            Cras auctor mollis cursus. Nulla facilisi. Curabitur vitae ipsum
+            mattis, finibus lectus ac, vehicula nisi. Duis est nisl, auctor
+            vitae placerat eu, vehicula quis ligula. Vestibulum mollis nisi
+            arcu, vitae sagittis magna placerat a. Class aptent taciti sociosqu
+            ad litora torquent per conubia nostra, per inceptos himenaeos.
+            Nullam sit amet cursus risus.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
