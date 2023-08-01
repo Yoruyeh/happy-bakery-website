ae8ac47becc0b287371fc59b0685a8705363f024
Yoruyeh <orchardsky31@gmail.com>
2023-08-01 16:58:25 +0800

    refactor: register page styles

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -13,61 +13,70 @@ const Register = () => {
 
         <p>OR</p>
 
-        <h5>Your Name</h5>
-        <div className={styles.inputWrapper}>
-          <TextInput type={'text'} placeholder={'First Name'} />
-        </div>
-        <div className={styles.inputWrapper}>
-          <TextInput type={'text'} placeholder={'Last Name'} />
-        </div>
-
-        <h5>Gender</h5>
-        <div className={styles.checkboxWrapper}>
+        <div className={styles.name}>
+          <h5>Your Name</h5>
           <div className={styles.inputWrapper}>
-            <CheckboxInput
-              type={'checkbox'}
-              name={'gender'}
-              value={'male'}
-              label={'Male'}
-            />
+            <TextInput type={'text'} placeholder={'First Name'} />
           </div>
           <div className={styles.inputWrapper}>
-            <CheckboxInput
-              type={'checkbox'}
-              name={'gender'}
-              value={'female'}
-              label={'Female'}
-            />
-          </div>
-          <div className={styles.inputWrapper}>
-            <CheckboxInput
-              type={'checkbox'}
-              name={'gender'}
-              value={'other'}
-              label={'Other'}
-            />
+            <TextInput type={'text'} placeholder={'Last Name'} />
           </div>
         </div>
 
-        <h5>Login Details</h5>
-        <div className={styles.inputWrapper}>
-          <TextInput type={'email'} placeholder={'Email'} />
+        <div className={styles.gender}>
+          <h5>Gender</h5>
+          <div className={styles.checkboxWrapper}>
+            <div className={styles.inputWrapper}>
+              <CheckboxInput
+                type={'checkbox'}
+                name={'gender'}
+                value={'male'}
+                label={'Male'}
+              />
+            </div>
+            <div className={styles.inputWrapper}>
+              <CheckboxInput
+                type={'checkbox'}
+                name={'gender'}
+                value={'female'}
+                label={'Female'}
+              />
+            </div>
+            <div className={styles.inputWrapper}>
+              <CheckboxInput
+                type={'checkbox'}
+                name={'gender'}
+                value={'other'}
+                label={'Other'}
+              />
+            </div>
+          </div>
         </div>
-        <div className={styles.inputWrapper}>
-          <TextInput type={'password'} placeholder={'Password'} />
+
+        <div className={styles.loginInfo}>
+          <h5>Login Details</h5>
+          <div className={styles.inputWrapper}>
+            <TextInput type={'email'} placeholder={'Email'} />
+          </div>
+          <div className={styles.inputWrapper}>
+            <TextInput type={'password'} placeholder={'Password'} />
+          </div>
         </div>
-        <div className={styles.inputWrapper}>
-          <CheckboxInput
-            type={'checkbox'}
-            label={`By clicking "Log In" you agree to our website Terms & Conditions,
+
+        <div className={styles.checkbox}>
+          <div className={styles.inputWrapper}>
+            <CheckboxInput
+              type={'checkbox'}
+              label={`By clicking "Log In" you agree to our website Terms & Conditions,
           Privacy Notice and Terms & Conditions.`}
-          />
-        </div>
-        <div className={styles.inputWrapper}>
-          <CheckboxInput
-            type={'checkbox'}
-            label={'Keep me logged in - applies to all log in options below.'}
-          />
+            />
+          </div>
+          <div className={styles.inputWrapper}>
+            <CheckboxInput
+              type={'checkbox'}
+              label={'Keep me logged in - applies to all log in options below.'}
+            />
+          </div>
         </div>
 
         <Button text={'REGISTER'} price={<ArrowForward />} />
