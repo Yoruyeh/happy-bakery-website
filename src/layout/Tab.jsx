ee8749e16d8d844327ea789df4956ffea6eacefa
Yoruyeh <orchardsky31@gmail.com>
2023-08-02 12:40:13 +0800

    feat: write member center layout- tab

--- src/layout/Tab.jsx
+++ src/layout/Tab.jsx
@@ -1,12 +1,30 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 import styles from './tab.module.scss'
 
 const Tab = () => {
   return (
-  <div className={styles.tab}>
-    Tab
-    <div><Outlet /></div>
-  </div>
+    <div className={styles.tab}>
+      <ul className={styles.tablist}>
+        <Link to="setting" className={`${styles.tabItem} ${styles.active}`}>
+          <li>Member Info</li>
+        </Link>
+        <Link to="orders" className={styles.tabItem}>
+          <li>Order History</li>
+        </Link>
+        <Link className={styles.tabItem}>
+          <li>Discount Voucher</li>
+        </Link>
+        <Link className={styles.tabItem}>
+          <li>Wishlist</li>
+        </Link>
+        <Link className={styles.tabItem}>
+          <li>Message</li>
+        </Link>
+      </ul>
+      <div className={styles.memberWrapper}>
+        <Outlet />
+      </div>
+    </div>
   )
 }
 
