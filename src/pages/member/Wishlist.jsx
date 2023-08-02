cc49ae1002ece509d516e8be5b6582e3689e9a06
Yoruyeh <orchardsky31@gmail.com>
2023-08-02 22:00:22 +0800

    feat: write wishlist page

--- src/pages/member/Wishlist.jsx
+++ src/pages/member/Wishlist.jsx
@@ -1,9 +1,55 @@
-import styles from './order.module.scss'
+import styles from './wishlist.module.scss'
+import ProductImg1 from '../../assets/images/chocolate-cake.jpeg'
+import ProductImg2 from '../../assets/images/scone.jpeg'
+import Button from '../../components/button/Button'
+import { Bin } from '../../assets/icons'
 
 const Wishlist = () => {
   return (
     <div className={styles.wishlist}>
-      <h3>Wishlist</h3>
+      <table className={styles.table}>
+        <thead className={styles.head}>
+          <tr>
+            <th>Product Image</th>
+            <th>Product Info</th>
+            <th>Product Price</th>
+            <th>Status</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody className={styles.body}>
+          <tr>
+            <td>
+              <img src={ProductImg1} alt="" />
+            </td>
+            <td>Chocolate Cake</td>
+            <td>$800.00</td>
+            <td>
+              <div className={styles.buttonWrapper}>
+                <Button text={'ADD TO CART'} />
+              </div>
+            </td>
+            <td>
+              <div className={styles.buttonWrapper}>
+                <Bin />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src={ProductImg2} alt="" />
+            </td>
+            <td>Chocolate Cake</td>
+            <td>$800.00</td>
+            <td>Restocking...</td>
+            <td>
+              <div className={styles.buttonWrapper}>
+                <Bin />
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
