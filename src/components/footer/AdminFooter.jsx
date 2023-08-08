767509bad716db75b0c8a6257470f3c3bf38c9af
Yoruyeh <orchardsky31@gmail.com>
2023-08-08 16:12:05 +0800

    feat: write admin footer component

--- src/components/footer/AdminFooter.jsx
+++ src/components/footer/AdminFooter.jsx
@@ -1,6 +1,19 @@
+import styles from './adminFooter.module.scss'
 
 const AdminFooter = () => {
-  return <div className="footer">AdminFooter</div>
+  return (
+    <div className={styles.adminFooter}>
+      <div className={styles.footerWrapper}>
+        <div className={styles.text}>© 2023 - Happy Bakery Dashboard</div>
+        <ul className={styles.links}>
+          <li>About</li>
+          <li>Careers</li>
+          <li>Policy</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default AdminFooter
