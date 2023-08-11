f86a18cb50e5b48203c702b5f109a4637ff36cc7
Yoruyeh <orchardsky31@gmail.com>
2023-08-11 19:46:45 +0800

    feat: write user login and register api

---
+++ src/api/user.auth.js
@@ -0,0 +1,72 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3000/api/users'
+
+export const UserSignIn = async ({ email, password }) => {
+  try {
+    const { data } = await axios.post(`${baseUrl}/signin`, {
+      email,
+      password
+    })
+
+    const { token } = data
+
+    if(token) {
+      localStorage.setItem('token', token)
+      return { success: true, ...data }
+    }
+
+    return data
+
+  } catch (error) {
+    console.error('[User Login Failed]: ', error)
+  }
+}
+
+export const UserSignUp = async ({
+  firstName,
+  lastName,
+  gender,
+  email,
+  password,
+  termsAgreement
+}) => {
+  try {
+    const { data } = axios.post(`${baseUrl}`, {
+      firstName,
+      lastName,
+      gender,
+      email,
+      password,
+      termsAgreement
+    })
+
+    const { token } = data
+
+    if (token) {
+      localStorage.setItem('token', token)
+      return { success: true, ...data }
+    }
+
+    return data
+
+  } catch (error) {
+    console.error('[User Register Failed]: ', error)
+  }
+}
+
+export const CheckPermission = async (token) => {
+  try {
+    const { data } = axios.get(baseUrl, {
+      headers: {
+        Authorization: 'Bearer ' + token
+      }
+    })
+    
+    return { success: true, ...data }
+
+  } catch (error) {
+    console.error('[Unauthorized]: ', error)
+  }
+}
+
