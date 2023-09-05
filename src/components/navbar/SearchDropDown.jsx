7ddd878694eafd4c3d52741b84329064f7fbb71f
Yoruyeh <orchardsky31@gmail.com>
2023-09-05 22:06:00 +0800

    feat: add handleviewproduct function on searched product

--- src/components/navbar/SearchDropDown.jsx
+++ src/components/navbar/SearchDropDown.jsx
@@ -1,20 +1,24 @@
-import { Link } from 'react-router-dom'
 import styles from './searchDropDown.module.scss'
+import { useProducts } from '../../context/ProductsContext'
 
 const SearchDropDown = ({ products, handleSeeAllClick }) => {
+  const { handleViewProductClick } = useProducts()
+
   return (
     <div className={styles.dropdown}>
       <h6>Products</h6>
       {products &&
         products.map((product) => (
-          <Link key={product.id}>
-            <div className={styles.searchItem}>
-              <div className={styles.searchItemImg}>
-                <img alt="" src={product.cover} />
-              </div>
-              <div className={styles.searchItemName}>{product.name}</div>
+          <div
+            className={styles.searchItem}
+            key={product.id}
+            onClick={() => handleViewProductClick(product.id, product.Category.name)}
+          >
+            <div className={styles.searchItemImg}>
+              <img alt="" src={product.cover} />
             </div>
-          </Link>
+            <div className={styles.searchItemName}>{product.name}</div>
+          </div>
         ))}
       {!products ? (
         <p>Not found</p>
