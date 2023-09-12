992507eee03bee635ab619d8f981cc7373e75d98
Yoruyeh <orchardsky31@gmail.com>
2023-09-12 12:09:09 +0800

    refactor: modify review cards content

--- src/components/card/ReviewCard.jsx
+++ src/components/card/ReviewCard.jsx
@@ -1,16 +1,13 @@
 import styles from './reviewCard.module.scss'
-import ReviewImg from '../../assets/images/chocolate-cake.jpeg'
 import { StarRate } from '../../assets/icons'
 
-const ReviewCard = () => {
+const ReviewCard = ({ avatar, title, text, productImg }) => {
   return (
     <div className={styles.card}>
       <div className={styles.body}>
         <div className={styles.text}>
-          <h5>Delicious!</h5>
-          <p>
-            I highly recommend shopping from Happy Bakery
-          </p>
+          <h5>{title}</h5>
+          <p>{text}</p>
           <div className={styles.rate}>
             <StarRate />
             <StarRate />
@@ -21,14 +18,11 @@ const ReviewCard = () => {
           </div>
         </div>
         <div className={styles.avatar}>
-          <img
-            src="https://images.unsplash.com/photo-1574158622682-e40e69881006?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80"
-            alt=""
-          />
+          <img src={avatar} alt="" />
         </div>
       </div>
       <div className={styles.image}>
-        <img src={ReviewImg} alt="" />
+        <img src={productImg} alt="" />
       </div>
     </div>
   )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,9 @@ import ReviewCard from '../../components/card/ReviewCard'
 import { useProducts } from '../../context/ProductsContext'
 import { Link } from 'react-router-dom'
 import { useEffect } from 'react'
+import ProductImg1 from '../../assets/images/chocolate-cake.jpeg'
+import ProductImg2 from '../../assets/images/croissant.jpeg'
+import ProductImg3 from '../../assets/images/scone.jpeg'
 
 const Home = () => {
   const { newProducts, handleNavItemClick } = useProducts()
@@ -60,12 +63,39 @@ const Home = () => {
       </div>
       <div className={styles.row}>
         <div className={styles.title}>
-          <h2>Reviews</h2>
+          <h2>5-star Reviews</h2>
         </div>
         <div className={styles.reviews}>
-          <ReviewCard />
-          <ReviewCard />
-          <ReviewCard />
+          <ReviewCard
+            avatar={
+              'https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1467&q=80'
+            }
+            title={'Absolutely divine!'}
+            text={
+              'This cake offers a luscious, melt-in-your-mouth experience with every bite. The depth of flavor and velvety texture is unmatched.'
+            }
+            productImg={ProductImg1}
+          />
+          <ReviewCard
+            avatar={
+              'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80'
+            }
+            title={'Exquisite and flaky perfection!'}
+            text={
+              'This croissant boasts a golden exterior that gives way to layers of buttery bliss. A true testament to artisan baking!'
+            }
+            productImg={ProductImg2}
+          />
+          <ReviewCard
+            avatar={
+              'https://images.unsplash.com/photo-1624561172888-ac93c696e10c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1589&q=80'
+            }
+            title={'An absolute delight!'}
+            text={
+              'This blueberry scone is bursting with juicy berries, enveloped in a tender, buttery crumb. Perfectly balanced sweetness makes it a morning must-have!'
+            }
+            productImg={ProductImg3}
+          />
         </div>
       </div>
     </div>
